ff60438731ee2d583b6c8685625065602002436a
SAPIENT\mmeghw <mmeghwal@sapient.com>
2017-03-07 20:55:23 +0530

    custom reverse pipe

--- src/app/views/pipes/pipes-routing.module.ts
+++ src/app/views/pipes/pipes-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AngularComponent } from './angular/angular.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  children: [{
+    path: '',
+    redirectTo:'pipe',
+    pathMatch: 'full'
+  },{
+    path: 'pipe',
+    component: AngularComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/views/pipes/pipes.module.ts
+++ src/app/views/pipes/pipes.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../../shared/shared.module';
+
 import { PipesRoutingModule } from './pipes-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AngularComponent } from './angular/angular.component';
+import { ReversePipe } from './angular/reverse.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    PipesRoutingModule
+    PipesRoutingModule,SharedModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, AngularComponent,ReversePipe]
 })
 export class PipesModule { }
